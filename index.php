cb884ec72ab22cb235fa54fdc5f4313753c26e46
Benoit Fiarda <fiarda33@live.fr>
2023-03-21 11:14:40 +0100

    ajout de index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+require('controllers/users/securityController.php') ;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<?php include 'templates/head.php';?>
+<body>
+    <?php include 'templates/navbar.php';?>
+</body>
+</html>
